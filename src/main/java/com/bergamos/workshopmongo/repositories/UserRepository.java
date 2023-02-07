d79cd8eaf6bce4dcb3c30db79371938458d11699
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-07 06:05:30 -0300

    Connecting to MongoDB with Repository and Service

---
+++ src/main/java/com/bergamos/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bergamos.workshopmongo.repositories;
+
+import com.bergamos.workshopmongo.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
