2f861a03048eaec07c8ef74d14b1d0245da442ca
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-08 05:08:53 -0300

    Delete User

--- src/main/java/com/bergamos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/bergamos/workshopmongo/resources/UserResource.java
@@ -42,6 +42,13 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 
 }
