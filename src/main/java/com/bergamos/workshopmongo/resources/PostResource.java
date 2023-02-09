663921ced99ec775128f740a9a84b054f9692248
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-09 04:59:44 -0300

    Simple query with @Query

--- src/main/java/com/bergamos/workshopmongo/resources/PostResource.java
+++ src/main/java/com/bergamos/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.bergamos.workshopmongo.resources;
 
 import com.bergamos.workshopmongo.entities.Post;
+import com.bergamos.workshopmongo.resources.util.URL;
 import com.bergamos.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,5 +21,14 @@ public class PostResource {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 
 }
--- src/main/java/com/bergamos/workshopmongo/services/PostService.java
+++ src/main/java/com/bergamos/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.bergamos.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,4 +19,9 @@ public class PostService {
         Optional<Post> obj = postRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Id is not existing."));
     }
+
+    public List<Post> findByTitle(String text) {
+        return  postRepository.searchTitle(text);
+    }
+
 }
